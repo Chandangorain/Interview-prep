9e2d5d4d80afe3417a302651545acd0aed7ce9ec
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-17 17:20:48 +0530

    blacklist

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,17 +7,17 @@ async function authUser(req, res, next) {
 
     const token = req.cookies.token
 
-    if (!token) {
+    if (!token) {                       // if no token present the mssge
         return res.status(401).json({
             message: "Token not provided."
         })
     }
 
-    const isTokenBlacklisted = await tokenBlacklistModel.findOne({
+    const isTokenBlacklisted = await tokenBlacklistModel.findOne({      // check if the token is blacklisted or not
         token
     })
 
-    if (isTokenBlacklisted) {
+    if (isTokenBlacklisted) {           // if blacklisted then token is invalids
         return res.status(401).json({
             message: "token is invalid"
         })
