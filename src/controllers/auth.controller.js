0cd2a0b3a13615a837c9970af2705c766f709b1a
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-17 17:15:34 +0530

    get-me cntroller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -116,9 +116,34 @@ async function logoutUserController(req, res) {
     })
 }
 
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access private
+ */
+async function getMeController(req, res) {
+
+    const user = await userModel.findById(req.user.id)
+
+
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+
+}
+
 
 module.exports = {
     registerUserController,
     loginUserController,
+    getMeController,
     logoutUserController
 }     //auth.controller ->auth.routes ->app.js ->server.js
+    
+  //auth.controller ->auth.routes ->app.js ->server.js
