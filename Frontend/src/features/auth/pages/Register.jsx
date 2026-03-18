0fe64769873c94c3cefd04c2f6a35b849e443e54
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-18 15:31:34 +0530

    register

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 
 const Register = () => {
+
+      const handleSubmit =  (e) => {
+        e.preventDefault()
+        
+    }
+
   return (
-    <div>
-      Register
-    </div>
+     <main>
+            <div className="form-container">
+                <h1>Register</h1>
+               <form onSubmit={handleSubmit}>
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            onChange={(e) => { setUsername(e.target.value) }}
+                            type="text" id="username" name='username' placeholder='Enter username' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name='email' placeholder='Enter email address' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name='password' placeholder='Enter password' />
+                    </div>
+
+                    <button className='button primary-button' >Register</button>
+
+                </form>
+                
+            </div>
+        </main>
   )
 }
 
