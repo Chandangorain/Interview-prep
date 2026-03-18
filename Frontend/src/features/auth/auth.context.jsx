83a9c53c0d7813227fb11b331a8bc83de4b7cd37
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-18 16:19:46 +0530

    authprovider wrap

---
+++ Frontend/src/features/auth/auth.context.jsx
@@ -0,0 +1,22 @@
+import { createContext,useState } from "react";
+
+export const AuthContext = createContext()
+
+
+export const AuthProvider = ({ children }) => { 
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    
+
+
+    return (
+        <AuthContext.Provider value={{user,setUser,loading,setLoading}} >
+            {children}
+        </AuthContext.Provider>
+    )
+
+    
+}
+  //this authprovider will be used to wrap the entire app in app.jsx so that we can access the auth context in any component of the app without having to pass props down manually at every level.
