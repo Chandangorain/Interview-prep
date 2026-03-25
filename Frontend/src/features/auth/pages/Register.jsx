c406165482a66b6f12d538636c8e2929857ad560
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-25 12:52:43 +0530

    two way binding

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import { useNavigate,Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
 
     const navigate = useNavigate()
+    const[username,setUsername]=useState("")  // this is two-way-binding . another part is onChange={(e)=>{setUsername(e.target.value)}} in username ,e mail, passowrd container in this code
+    const[email,setEmail]=useState("")      //username → stores current input value setUsername() → updates that value
+    const[password,setPassword]=useState("")
 
-      const handleSubmit =  (e) => {
-        e.preventDefault()
-        
+    const{loading,handleRegister}=useAuth()     //get loading , handleRegister from the useAuth() hook
+
+      const handleSubmit =  async (e) => {  // when user clicks register button
+        e.preventDefault()      // should not refresh page
+        await handleRegister({username,email,password}) // send user,email,pass to useAuth
+        navigate("/")       // redirect to home pge
+    }
+
+    if(loading){
+        return(<main><h1>Loading..</h1></main>)
     }
 
   return (
@@ -18,15 +29,22 @@ const Register = () => {
 
                     <div className="input-group">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name='username' placeholder='Enter username' />
+                        <input 
+                        onChange={(e)=>{setUsername(e.target.value)}}  // two way binding whenever user types something update username state with that value
+                        
+                        type="text" id="username" name='username' placeholder='Enter username' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name='email' placeholder='Enter email address' />
+                        <input 
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                        type="email" id="email" name='email' placeholder='Enter email address' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name='password' placeholder='Enter password' />
+                        <input 
+                        onChange={(e)=>{setPassword(e.target.value)}}
+                        type="password" id="password" name='password' placeholder='Enter password' />
                     </div>
 
                     <button className='button primary-button' >Register</button>
