38ebb7a259633655c49270de0e8c78e3c529bb3e
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-26 17:53:43 +0530

    technicalquestion schema

--- Backend/src/models/interviewReport.model.js
+++ Backend/src/models/interviewReport.model.js
@@ -55,6 +55,53 @@ const technicalQuestionSchema = new mongoose.Schema({
     _id: false      // no need of id so false
 })
 
+
+const behavioralQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [ true, "Technical question is required" ]
+    },
+    intention: {
+        type: String,
+        required: [ true, "Intention is required" ]
+    },
+    answer: {
+        type: String,
+        required: [ true, "Answer is required" ]
+    }
+}, {
+    _id: false
+})
+
+const skillGapSchema = new mongoose.Schema({
+    skill: {
+        type: String,
+        required: [ true, "Skill is required" ]
+    },
+    severity: {
+        type: String,
+        enum: [ "low", "medium", "high" ],
+        required: [ true, "Severity is required" ]
+    }
+}, {
+    _id: false
+})
+
+const preparationPlanSchema = new mongoose.Schema({
+    day: {
+        type: Number,
+        required: [ true, "Day is required" ]
+    },
+    focus: {
+        type: String,
+        required: [ true, "Focus is required" ]
+    },
+    tasks: [ {
+        type: String,
+        required: [ true, "Task is required" ]
+    } ]
+})
+
 const interviewReportSchema = new mongoose.Schema({
     jobDescription: {
         type: String,
